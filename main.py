2a563d31723f693e539f9bb151d33c7ecbc1f079
Ethanol2 <E.Colucci@outlook.com>
2025-06-19 10:49:37 -0400

    Added player object

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame;
 from constants import *;
+from player import *;
 
 def main():
     print("Starting Asteroids!");
     print(f"Screen width: {SCREEN_WIDTH}");
     print(f"Screen height: {SCREEN_HEIGHT}");
 
+    # System Stuff
     pygame.init();
+    pyClock = pygame.time.Clock();
+    deltaTime = 0;
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT));
+
+    # Game Objects
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
     while (True):
         
@@ -16,7 +23,11 @@ def main():
                 return;
 
         screen.fill((0,0,0));
+
+        player.draw(screen);
+
         pygame.display.flip();
+        deltaTime = pyClock.tick(60) / 1000;
 
 if (__name__ == "__main__"):
     main();
